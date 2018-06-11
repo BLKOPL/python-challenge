624a158c498a36a8ecedc450f53f585086a77f6c
Maria <maria.seneca@protonmail.com>
2018-06-11 15:57:45 -0400

    final solutions for PyBank datasets

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,18 +2,18 @@ import os
 import csv
 
 #establish root, data, and output 
-root_path = os.path.join(os.getcwd(),".")
+root_path = os.path.join(os.getcwd(), ".")
 data_path = os.path.join(root_path, "raw_data")
 output_path = os.path.join(root_path, "output")
 
 #iterate through all csv to apply standard code
-#This allows the code to apply to any new .csvs added
+#This allows the code to apply to any new .csv added
 filepaths = []
 for file in os.listdir(data_path):
     if file.endswith(".csv"):
         filepaths.append(os.path.join(data_path, file))
 
-#Applying DictReader method to standardize python code for all files in filepath list
+#Applying .DictReader method to standardize python code for all files in filepath list
 for file in filepaths:
     tot_revenue = 0
     month_count = 0
@@ -43,3 +43,34 @@ for file in filepaths:
         first_row_revdiff = first_row.get("rev_diff")
         rev_change = rev_change - first_row_revdiff
         avg_change = int(rev_change/(month_count-1))
+
+        #grab the filename from the original path 
+        #note that _, gets rid of the path and , getss rid of the .csv
+        _, filename = os.path.split(file)
+        filename, _ = filename.split(".csv")
+
+        #print results to terminal using template literals
+        print(
+            f"Financial Analysis = {filename}\n"
+            f"----------------------------\n"
+            f"Total Months = {month_count}\n"
+            f"Total Revenue = {tot_revenue}\n"
+            f"Average Revenue Change = {avg_change}\n"
+            f"Greatest Increase In Revenue: {increase_date} (${increase_revdiff}\n"
+            f"Greatest Decrease In Revenue: {decrease_date} (${decrease_revdiff}\n"
+        )
+
+        #generate .txt file for datasets and export that to output folder.
+        text_path = os.path.join(output_path, filename + ".txt")
+        with open(text_path, "w") as text_file:
+            text_file.write(
+            f"Financial Analysis = {filename}\n"
+            f"----------------------------\n"
+            f"Total Months = {month_count}\n"
+            f"Total Revenue = {tot_revenue}\n"
+            f"Average Revenue Change = {avg_change}\n"
+            f"Greatest Increase In Revenue: {increase_date} (${increase_revdiff}\n"
+            f"Greatest Decrease In Revenue: {decrease_date} (${decrease_revdiff}\n"
+        )
+
+
